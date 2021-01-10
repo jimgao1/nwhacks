8d4eab69ab712e1143f9d3b69185149e1d6a22b5
Jim Gao <jim@jimgao.tk>
2021-01-10 02:54:01 -0800

    Canvas offset now works

--- 01_body_from_image.py
+++ 01_body_from_image.py
@@ -13,7 +13,6 @@ from fuckbuffer import FuckBuffer
 
 draw_color = [(0, 255, 0), (69,69,69)]
 
-
 def dist(p1, p2):
     dx = p1[0] - p2[0]
     dy = p1[1] - p2[1]
@@ -86,9 +85,12 @@ try:
 
     finger_pos = dict()
 
-    cancer = [np.zeros((480, 640, 1)), np.zeros((480, 640, 1))]
-    cancer[0].fill(0)
-    cancer[1].fill(0)
+    # dimension of video stream
+    height, width = 480, 640
+
+    # dimension of canvas
+    canvas_view_corner = np.array((height//2, width//2), dtype=np.int32)
+    cancer = [np.zeros((height*2, width*2, 1)), np.zeros((height*2, width*2, 1))]
 
     previous_point = dict()
 
@@ -155,7 +157,8 @@ try:
                 for point_id, point in enumerate(person):
                     if point[0] == 0.0 and point[1] == 0.0: continue
                     model_img = cv2.putText(model_img, "%d" % point_id,
-                                    (point[0], point[1]), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 255))
+                                    (point[0], point[1]),
+                                    cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 255))
 
         if datum.handKeypoints is not None:
             hand_colors = [(0, 255, 255), (0, 128, 128)]
@@ -213,13 +216,16 @@ try:
 
                             if cur_clicked or click_debouncer.avg() >= 1.0:
                                 model_img = cv2.circle(model_img, (point[0], point[1]), 2, hand_colors[hand_id], 2)
-                                imageToProcess = cv2.circle(imageToProcess, (point[0], point[1]), 2, hand_colors[hand_id], 2)
-                                cancer[hand_id] = cv2.circle(cancer[hand_id], (point[0], point[1]), 2, (255,), 2)
+                                # imageToProcess = cv2.circle(imageToProcess, (point[0], point[1]), 2, hand_colors[hand_id], 2)
+
+                                cancer[hand_id] = cv2.circle(cancer[hand_id],
+                                                             tuple(np.array((point[0], point[1]), dtype=np.int32) + canvas_view_corner), 2, (255,), 2)
+
 
 
                                 if hand_id in previous_point:
-                                    cancer[hand_id] = cv2.line(cancer[hand_id], previous_point[hand_id], smoothed,
-                                                               (255, ), 2)
+                                    cancer[hand_id] = cv2.line(cancer[hand_id], tuple(previous_point[hand_id] + canvas_view_corner),
+                                        tuple(smoothed + canvas_view_corner), (255, ), 2)
 
                             # clear motion smoothing buffer on release
                             # if click_debouncer.avg() < 2.0:
@@ -232,20 +238,37 @@ try:
                             # imageToProcess = np.where(canvas < 255.0, imageToProcess, canvas)
         # imageToProcess = (imageToProcess + canvas) // 2
 
-        for i in [0, 1]: cancer[i] = np.clip(cancer[i], 0, 255).astype('uint8')
-        combined_cancer = np.clip(sum([draw_color[i] * cancer[i] for i in [0, 1]]), 0, 255).astype('uint8')
 
-        combined = imageToProcess - cv2.bitwise_and(imageToProcess, imageToProcess, mask=cv2.bitwise_or(cancer[0], cancer[1]))
+        cancer_disp = [
+            np.clip(cancer[i][canvas_view_corner[1]:canvas_view_corner[1] + height, canvas_view_corner[0]:canvas_view_corner[0] + width, :], 0, 255).astype('uint8')
+            for i in [0, 1]
+        ]
+
+        print(cancer_disp[0].shape, cancer_disp[1].shape)
+
+        combined_cancer = np.clip(sum([draw_color[i] * cancer_disp[i] for i in [0, 1]]), 0, 255).astype('uint8')
+
+        combined = imageToProcess - cv2.bitwise_and(imageToProcess, imageToProcess, mask=cv2.bitwise_or(cancer_disp[0], cancer_disp[1]))
         combined = combined + combined_cancer
         combined = np.clip(combined, 0, 255).astype('uint8')
 
         cv2.imshow("frame", cv2.flip(combined, 1))
         cv2.imshow("output", model_img)
         cv2.imshow("canvas", cv2.flip(combined_cancer, 1))
+        # cv2.imshow("c0", cancer[0])
+        # cv2.imshow("c1", cv2.rectangle(255 - cancer[1], tuple(canvas_view_corner),
+        #                                tuple(canvas_view_corner + np.array((640, 480))),
+        #                                (0, 127, 0)))
         key = cv2.waitKey(1)
 
         # key binds to erase canvas
         if key == ord('l') or key == ord('c'): cancer[0].fill(0)
         if key == ord('r') or key == ord('c'): cancer[1].fill(0)
+
+        # translating the canvas view
+        if key == ord('j'): canvas_view_corner[0] += 10
+        if key == ord('k'): canvas_view_corner[0] -= 10
+        if key == ord('h'): canvas_view_corner[1] -= 10
+        if key == ord('l'): canvas_view_corner[1] += 10
 except Exception as e:
     raise e

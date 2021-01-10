8b6f85e48ce70f4a5fc27f14df685ab30a3a4d80
Jim Gao <jim@jimgao.tk>
2021-01-09 21:12:26 -0800

    Semi functional I guess

--- 01_body_from_image.py
+++ 01_body_from_image.py
@@ -8,6 +8,10 @@ from sys import platform
 import argparse
 import numpy as np
 
+
+draw_color = [(0, 255, 0), (69,69,69)]
+
+
 def dist(p1, p2):
     dx = p1[0] - p2[0]
     dy = p1[1] - p2[1]
@@ -71,15 +75,20 @@ try:
 
     finger_pos = dict()
 
-    canvas = np.zeros((480, 640, 3))
-    canvas.fill(255)
+    cancer = [np.zeros((480, 640, 1)), np.zeros((480, 640, 1))]
+    cancer[0].fill(0)
+    cancer[1].fill(0)
+
+    previous_point = dict()
+
+    last_frame_time = 0
 
     while True:
         # Process Image
         ret, imageToProcess = cap.read()
 
         if not ret: print("Failed to capture image")
-        print(imageToProcess.shape)
+
         # imageToProcess = cv2.imread(args[0].image_path)
         datum.cvInputData = imageToProcess
 
@@ -87,8 +96,11 @@ try:
         opWrapper.emplaceAndPop(op.VectorDatum([datum]))
         end_time = int(round(time.time() * 1000))
 
-        print("frame time: %d ms" % (end_time - start_time))
         model_img = datum.cvOutputData
+
+        cv2.putText(model_img, "%.2f fps" % (1000 / (start_time-last_frame_time)),
+                    (10, 30), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 255, 0))
+        last_frame_time = start_time
 
         # for point in datum.poseKeypoints
         if draw_pose_ids and datum.poseKeypoints is not None:
@@ -100,13 +112,16 @@ try:
                                     (point[0], point[1]), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 255))
 
         if datum.handKeypoints is not None:
-            hand_colors = [(0, 255, 255), (255, 0, 255)]
+            hand_colors = [(0, 255, 255), (0, 128, 128)]
             for hand_id, hands in enumerate(datum.handKeypoints):
                 if hands is None: continue
+
                 num_people = hands.shape[0]
                 for person_id in range(num_people):
                     for point_id in range(21):
                         point = hands[person_id][point_id]
+                        if point[0] == 0 and point[1] == 0: continue
+
                         if draw_hand_ids:
                             model_img = cv2.putText(model_img, "%c%d" % ('L' if hand_id == 0 else 'R', point_id),
                                                     (point[0], point[1]), cv2.FONT_HERSHEY_DUPLEX,
@@ -121,7 +136,7 @@ try:
                             d2 = dist(finger_pos[7], finger_pos[8])
                             print(d1, d2, d2/d1)
 
-                            clicked = d2/d1 < 0.3
+                            clicked = d2/d1 < 1.2
 
                         # make right-index a special point
                         if draw_pointer and point_id == 8:
@@ -129,17 +144,38 @@ try:
                                                    (0, 0, 255) if clicked else (255, 255, 255),
                                                    4)
 
-                        if hand_id == 1 and point_id == 8 and clicked:
-                            model_img = cv2.circle(model_img, (point[0], point[1]), 2, (255, 255, 0), 2)
-                            imageToProcess = cv2.circle(imageToProcess, (point[0], point[1]), 2, (255, 255, 0), 2)
-                            canvas = cv2.circle(canvas, (point[0], point[1]), 2, (255, 255, 0), 2)
+                        if point_id == 8:
+                            if clicked:
+                                model_img = cv2.circle(model_img, (point[0], point[1]), 2, hand_colors[hand_id], 2)
+                                imageToProcess = cv2.circle(imageToProcess, (point[0], point[1]), 2, hand_colors[hand_id], 2)
+                                cancer[hand_id] = cv2.circle(cancer[hand_id], (point[0], point[1]), 2, (255,), 2)
 
-        imageToProcess = np.where(canvas < 255.0, imageToProcess, canvas)
+                                if hand_id in previous_point:
+                                    cancer[hand_id] = cv2.line(cancer[hand_id], previous_point[hand_id], (point[0], point[1]), (255, ), 2)
+
+                            previous_point[hand_id] = (point[0], point[1])
+
+
+                            # imageToProcess = np.where(canvas < 255.0, imageToProcess, canvas)
         # imageToProcess = (imageToProcess + canvas) // 2
 
-        cv2.imshow("frame", imageToProcess)
+        for i in [0, 1]: cancer[i] = np.clip(cancer[i], 0, 255).astype('uint8')
+        combined_cancer = np.clip(sum([draw_color[i] * cancer[i] for i in [0, 1]]), 0, 255).astype('uint8')
+
+        combined = imageToProcess - cv2.bitwise_and(imageToProcess, imageToProcess, mask=cv2.bitwise_or(cancer[0], cancer[1]))
+        combined = combined + combined_cancer
+        combined = np.clip(combined, 0, 255).astype('uint8')
+
+
+        print("combined shape", combined.shape)
+
+        cv2.imshow("frame", cv2.flip(combined, 1))
         cv2.imshow("output", model_img)
-        cv2.imshow("canvas", canvas)
-        cv2.waitKey(1)
+        cv2.imshow("canvas", cv2.flip(combined_cancer, 1))
+        key = cv2.waitKey(1)
+
+        # key binds to erase canvas
+        if key == ord('l') or key == ord('c'): cancer[0].fill(0)
+        if key == ord('r') or key == ord('c'): cancer[1].fill(0)
 except Exception as e:
     raise e

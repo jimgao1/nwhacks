a55b299dd796aef6d3369b9e014d84ce4c59fa0f
uoftcspost <46848142+uoftcspost@users.noreply.github.com>
2021-01-10 06:17:49 -0500

    merged fuckbuffer to main file

--- 01_body_from_image.py
+++ 01_body_from_image.py
@@ -9,7 +9,46 @@ import argparse
 import numpy as np
 import collections
 
-from fuckbuffer import FuckBuffer
+class FuckBuffer:
+    def __init__(self, queue_size=10, gamma=0.5):
+        self.count = 0
+        self.queue_size = queue_size
+        self.gamma = gamma
+        self.sum = 0.0
+        self.wsum = 0.0
+        self.c = collections.deque()
+
+    def append(self, e):
+        e = float(e)
+
+        self.c.append(e)
+        self.count += 1
+        self.sum += e
+        self.wsum = self.gamma * self.wsum + e
+
+        if self.count > self.queue_size:
+            val = self.c.popleft()
+            self.sum -= val
+            self.wsum -= val * self.gamma**(self.count+1)
+            self.count -= 1
+
+    def pop(self):
+        self.count -= 1
+        self.sum -= self.c.popleft()
+
+    def avg(self):
+        return self.sum / self.count
+
+    def wavg(self):
+        # if self.count:
+        #     return self.wsum ** (1/self.count)
+        total_weight = (1 - self.gamma ** (self.count + 1)) / (1 - self.gamma)
+        return self.wsum / total_weight
+
+    def clear(self):
+        self.count = 0
+        self.wsum = self.sum = 0.0
+        self.c.clear()
 
 draw_color = [(0, 255, 0), (69,69,69)]
 
@@ -36,7 +75,6 @@ try:
     parser.add_argument("--image_path", default="../examples/media/COCO_val2014_000000000192.jpg",
                         help="Process an image. Read all standard formats (jpg, png, bmp, etc.).")
     args = parser.parse_known_args()
-
     # Custom Params (refer to include/openpose/flags.hpp for more parameters)
     params = dict()
     params["model_folder"] = "models/"
